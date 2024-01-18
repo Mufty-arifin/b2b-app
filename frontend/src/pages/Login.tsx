e3373c957d58a36ba00d376ca5a69fe7ecb8116b
Septian Maulana <septiandev028@gmail.com>
2024-01-19 00:32:22 +0700

    feat: create register page include logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Spinner } from "../components";
 
 const Login = () => {
   const [loginForm, setLoginForm] = useState({
@@ -58,25 +59,21 @@ const Login = () => {
             ) : (
               <>
                 <div>
-                  <img
-                    className="h-10 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                  />
+                  <span className="flex justify-center">
+                    <img
+                      className="h-10 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt="Your Company"
+                    />
+
+                    <p className="ml-2 text-3xl font-bold leading-9 tracking-tight text-gray-900">
+                      B2B Portal
+                    </p>
+                  </span>
 
                   <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                    Login to your account
                   </h2>
-
-                  <p className="mt-2 text-sm leading-6 text-gray-500">
-                    Not a member?{" "}
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Start a 14 day free trial
-                    </a>
-                  </p>
                 </div>
 
                 <div className="mt-10">
@@ -155,27 +152,13 @@ const Login = () => {
                           type="submit"
                           className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                          Sign in
+                          Login
                         </button>
                       </div>
                     </form>
                   </div>
 
                   <div className="mt-10">
-                    <div className="relative">
-                      <div
-                        className="absolute inset-0 flex items-center"
-                        aria-hidden="true"
-                      >
-                        <div className="w-full border-t border-gray-200" />
-                      </div>
-                      <div className="relative flex justify-center text-sm font-medium leading-6">
-                        <span className="bg-white px-6 text-gray-900">
-                          Or continue with
-                        </span>
-                      </div>
-                    </div>
-
                     <div className="mt-6 grid grid-cols-2 gap-4">
                       <a
                         href="#"
@@ -229,6 +212,18 @@ const Login = () => {
                         </span>
                       </a>
                     </div>
+                  </div>
+
+                  <div className="mt-10">
+                    <p className="mt-2 text-sm leading-6 text-gray-500">
+                      Don't have an account?{" "}
+                      <Link
+                        to="/register"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                      >
+                        Register
+                      </Link>
+                    </p>
                   </div>
                 </div>
               </>
