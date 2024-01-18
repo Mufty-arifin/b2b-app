6c2983027372f3f3363d1d8808c69198d473aaff
Septian Maulana <septiandev028@gmail.com>
2024-01-18 11:23:39 +0700

    feat: create user model and migration using sequelize cli

---
+++ backend/db/models/user.js
@@ -0,0 +1,62 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Email is required",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+          isEmail: {
+            msg: "Email format is invalid",
+          },
+        },
+        unique: {
+          msg: "Email already exists",
+        },
+      },
+      username: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password is required",
+          },
+          notEmpty: {
+            msg: "Password is required",
+          },
+          len: {
+            args: [5],
+            msg: "Password length min is 5",
+          },
+        },
+      },
+      phone: DataTypes.STRING,
+      address: DataTypes.TEXT,
+      isActive: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      paranoid: true, // soft delete is active
+      modelName: "User",
+    }
+  );
+  return User;
+};
